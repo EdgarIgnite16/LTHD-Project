af872f350a455edd2c11cc99dcfdb257c05a4df9
Qwerty9927 <tanvo9927@gmail.com>
2024-04-21 17:02:00 +0700

    update

--- projects/BaiTapLamLai/btap4/lib/main.dart
+++ projects/BaiTapLamLai/btap4/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:btap4/screens/home_screen.dart';
-import 'package:btap4/screens/state/GlobalState.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
